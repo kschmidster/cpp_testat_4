25186614ea00c418ffb42300c323870f67b981b6
kschmidi <kschmidi@hsr.ch>
2014-11-28 11:30:45 +0100

    initial commit

---
+++ src/Test.cpp
@@ -0,0 +1,177 @@
+#include "cute.h"
+#include "ide_listener.h"
+#include "xml_listener.h"
+#include "cute_runner.h"
+
+#include "dynArray.h"
+
+void test_create_dyArray_no_args() {
+	dynArray<int> test1 { };
+	dynArray<char> test2 { };
+	dynArray<std::string> test3 { };
+
+	ASSERT(test1.empty());
+	ASSERT(test2.empty());
+	ASSERT(test3.empty());
+}
+
+void test_create_dyArray_init_list() {
+	dynArray<int> test { 1, 2, 3, 4, 5 };
+
+	ASSERT(!test.empty());
+	ASSERT_EQUAL(5, test.size());
+}
+
+void test_create_dyArray_eql_val() {
+	dynArray<int> test(3, 42);
+
+	ASSERT(!test.empty());
+	ASSERT_EQUAL(3, test.size());
+}
+
+void test_create_dyArray_istr_iter() {
+	std::istringstream in { "bla bla just some text..." };
+	std::istringstream end { };
+	dynArray<std::string> test { in, end };
+
+	ASSERT(!test.empty());
+	ASSERT_EQUAL(5, test.size());
+}
+
+void test_at_func() {
+	dynArray<int> test(3, 42);
+
+	ASSERT_EQUAL(42, test.at(2));
+}
+
+void test_at_func_lval() {
+	dynArray<double> test(3, 3.14);
+
+	test.at(2) = 2.78;
+
+	ASSERT_EQUAL(2.78, test.at(2));
+}
+
+void test_at_func_neg_index(){
+	dynArray<std::string> test { "this", "is", "a", "test" };
+
+	ASSERT_EQUAL("test", test.at(-1));
+	ASSERT_EQUAL("a", test.at(-2));
+	ASSERT_EQUAL("is", test.at(-3));
+	ASSERT_EQUAL("this", test.at(-4));
+}
+
+void test_oper_sqr_brac_lval() {
+	dynArray<std::string> test { "hello this is a text" };
+
+	test[test.size()-1] = "test";
+
+	ASSERT_EQUAL("test", test[test.size() - 1]);
+}
+
+void test_oper_sqr_brac_neg_index() {
+	dynArray<int> test { 1, 3, 7, 42, 9 };
+
+	ASSERT_EQUAL(42, test[-2]);
+}
+
+void test_func_front() {
+	dynArray<double> test { 1.0, 2.0, 3.0, 4.0 };
+
+	ASSERT_EQUAL(1.0, test.front());
+}
+
+void test_func_front_lval() {
+	dynArray<std::string> test { "hello i'm writing cute tests" };
+
+	test.front() = "hi";
+
+	ASSERT_EQUAL("hi", test.front());
+}
+
+void test_func_back() {
+	dynArray<int> test { 1, 3, 5, 7, 9 };
+
+	ASSERT_EQUAL(9, test.back());
+}
+
+void test_func_back_lval() {
+	dynArray<int> test { 2, 4, 6, 8 };
+
+	test.back() = 42;
+
+	ASSERT_EQUAL(42, test.back());
+}
+
+void test_func_capacity() {
+	using vec = std::vector<int>;
+	dynArray<vec> test { vec { 1, 2, 4 }, vec { 4, 5, 6 }, vec { 7, 8, 9 } };
+
+	ASSERT_EQUAL(3, test.capacity());
+	ASSERT_EQUAL(3, test.size());
+}
+
+void test_func_clear() {
+	dynArray<double> test { 1.0, 2.0, 3.0, 4.0 };
+
+	test.clear();
+
+	ASSERT(!test.size());
+	ASSERT(test.empty());
+	ASSERT_EQUAL(4, test.capacity());
+}
+
+void test_func_push_back(){
+	dynArray<std::string> test {"hallo"};
+
+	test.push_back("this");
+	test.push_back("is");
+	test.push_back("a");
+
+	ASSERT_EQUAL("a", test.at(-1));
+	ASSERT_EQUAL("is", test.at(-2));
+	ASSERT_EQUAL("this", test.at(-3));
+	ASSERT_EQUAL("hallo", test.at(-4));
+}
+
+void test_func_pop_back(){
+	using pair = std::pair<int, std::string>;
+	dynArray<pair> test { { 3, "3"}, {5, "5"}, { 7, "7"} };
+
+	test.pop_back();
+
+	ASSERT_EQUAL(2, test.size());
+	ASSERT_EQUAL(3, test.capacity());
+}
+
+void runAllTests(int argc, char const *argv[]) {
+	cute::suite s { };
+
+	//My tests
+	s.push_back(CUTE(test_create_dyArray_no_args));
+	s.push_back(CUTE(test_create_dyArray_init_list));
+	s.push_back(CUTE(test_create_dyArray_eql_val));
+	s.push_back(CUTE(test_create_dyArray_istr_iter));
+	s.push_back(CUTE(test_at_func));
+	s.push_back(CUTE(test_at_func_lval));
+	s.push_back(CUTE(test_at_func_neg_index));
+	s.push_back(CUTE(test_oper_sqr_brac_lval));
+	s.push_back(CUTE(test_oper_sqr_brac_neg_index));
+	s.push_back(CUTE(test_func_front));
+	s.push_back(CUTE(test_func_front_lval));
+	s.push_back(CUTE(test_func_back));
+	s.push_back(CUTE(test_func_back_lval));
+	s.push_back(CUTE(test_func_capacity));
+	s.push_back(CUTE(test_func_clear));
+	s.push_back(CUTE(test_func_push_back));
+	s.push_back(CUTE(test_func_pop_back));
+
+	cute::xml_file_opener xmlfile(argc, argv);
+	cute::xml_listener<cute::ide_listener<> > lis(xmlfile.out);
+	cute::makeRunner(lis, argc, argv)(s, "AllTests");
+}
+
+int main(int argc, char const *argv[]) {
+	runAllTests(argc, argv);
+	return 0;
+}
---
+++ src/dynArray.h
@@ -0,0 +1,126 @@
+#ifndef DYNARRAY_H_
+#define DYNARRAY_H_
+
+#include <vector>
+#include <iterator>
+
+template<typename T>
+class dynArray {
+	using value_type = T;
+	using reference = value_type&;
+	using const_reference =  const value_type&;
+	using vec = std::vector<value_type>;
+	using size_type = typename vec::size_type;
+	using iterator = typename vec::iterator;
+	using const_iterator = typename vec::const_iterator;
+	using reverse_iterator = typename vec::reverse_iterator;
+	using const_reverse_iterator = typename vec::const_reverse_iterator;
+
+	vec myArray;
+
+public:
+	//ctor
+	dynArray() : myArray { } { }
+	dynArray(std::initializer_list<value_type> list) : myArray { } {
+			myArray.insert(myArray.cend(), list.begin(), list.end());
+	}
+	dynArray(size_type size, value_type const &value) : myArray { } {
+		myArray.insert(myArray.cbegin(), size, value);
+	}
+	dynArray(std::istream_iterator<value_type> begin, std::istream_iterator<value_type> end) :
+		myArray { begin, end } { }
+
+	//access
+	reference at(int index) {
+		//TODO check implementation
+		if (index >= 0) {
+			return myArray.at(index);
+		} else {
+			return myArray.at(myArray.size() + index);
+		}
+	}
+
+	const_reference at(int index) const {
+		//TODO check implementation
+		if (index >= 0) {
+			return const_reference { myArray.at(index) };
+		} else {
+			return const_reference { myArray.at(myArray.size() + index) };
+		}
+	}
+
+	reference operator[](int index) {
+		//TODO check implementation
+		if (index >= 0) {
+			return reference { myArray.at(index) };
+		} else {
+			return reference { myArray.at(myArray.size() + index) };
+		}
+	}
+
+	const_reference operator[](int index) const {
+		//TODO check implementation
+		if (index >= 0) {
+			return myArray[index];
+		} else {
+			return myArray[myArray.size() + index];
+		}
+	}
+
+	reference front() { return myArray.front(); }
+
+	const_reference front() const {
+		return const_reference { myArray.front() };
+	}
+
+	reference back() { return myArray.back(); }
+
+	const_reference back() const {
+		return const_reference { myArray.back() };
+	}
+
+	//Iterators TODO check iterator
+	iterator begin() noexcept { return myArray.begin(); }
+	const_iterator begin() const noexcept { return myArray.begin(); }
+	const_iterator cbegin() const noexcept { return myArray.cbegin(); }
+	reverse_iterator rbegin() noexcept { return myArray.rbegin(); }
+	const_reverse_iterator rbegin() const noexcept { return myArray.rbegin(); }
+	const_reverse_iterator crbegin() const noexcept { return myArray.crbegin(); }
+	iterator end() noexcept { return myArray.end(); }
+	const_iterator end() const noexcept { return myArray.end(); }
+	const_iterator cend() const noexcept { return myArray.cend(); }
+	reverse_iterator rend() noexcept { return myArray.rend(); }
+	const_reverse_iterator rend() const noexcept { return myArray.rend(); }
+	const_reverse_iterator crend() const noexcept { return myArray.crend(); }
+
+	//Capacity
+	bool empty() const noexcept { return myArray.empty(); }
+	size_type size() const noexcept { return myArray.size(); }
+	size_type capacity() const noexcept { return myArray.capacity(); }
+
+	//Modifiers TODO check iterator
+	void clear() noexcept { myArray.clear(); }
+	iterator erase(iterator pos) { return myArray.erase(pos); }
+	const_iterator erase(const_iterator pos) { return myArray.erase(pos); }
+	iterator erase(iterator first, iterator last) {
+		return myArray.erase(first, last);
+	}
+	const_iterator erase(const_iterator first, const_iterator last) {
+		return myArray.erase(first, last);
+	}
+	void push_back(const_reference value) { myArray.push_back(value); }
+	void pop_back() { myArray.pop_back(); }
+	void resize(size_type count, value_type value = value_type()) {
+		myArray.resize(count, value);
+	}
+	void resize(size_type count) { myArray.resize(count); }
+	void resize(size_type count, const_reference value) {
+		myArray.resize(count, value);
+	}
+
+	static dynArray<value_type>& makedynArray(std::initializer_list<value_type> list) {
+		return dynArray { list };
+	}
+};
+
+#endif /* DYNARRAY_H_ */
